8e82b8181e759f23724e52f36a6a8a83762d2f4f
juanluisavila-aircury <juanluis.avila@aircury.com>
2023-04-24 08:38:56 +0200

    User service

--- api/src/Entity/User.php
+++ api/src/Entity/User.php
@@ -11,6 +11,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Table(name: '`user`', schema: 'edmundoschema')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
+    public function __construct(String $email){
+        $this->email = $email;
+    }
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
